6694ddf6b09c1996d7d092df4cd527788d1f839b
Samkwizera <s.ihimbazwe@alustudent.com>
2026-01-22 18:52:40 +0200

    Update linear algebra files

--- math/linear_algebra/0-slice_me_up.py
+++ math/linear_algebra/0-slice_me_up.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
-the_middle = []
-for i in matrix:
-    the_middle.append(i[2:4])
-print("The middle columns of the matrix are: {}".format(the_middle))
+arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
+arr1 = arr[:2]
+arr2 = arr[-5:]
+arr3 = arr[1:6]
+print("The first two numbers of the array are: {}".format(arr1))
+print("The last five numbers of the array are: {}".format(arr2))
+print("The 2nd through 6th numbers of the array are: {}".format(arr3))
