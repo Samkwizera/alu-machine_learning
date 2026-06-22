9ac8354910f2316b2f707cb206644cf3e1e10ba7
Samkwizera <s.ihimbazwe@alustudent.com>
2026-06-22 22:53:01 +0200

    Make user location script executable

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-
-
-""" Return list of ships"""
+"""Print the location of a GitHub user."""
 
 import requests
 import sys
@@ -12,14 +10,12 @@ if __name__ == "__main__":
     res = requests.get(sys.argv[1])
 
     if res.status_code == 403:
-        rate_limit = int(res.headers.get('X-Ratelimit-Reset'))
+        rate_limit = int(res.headers.get("X-Ratelimit-Reset"))
         current_time = int(time.time())
         diff = (rate_limit - current_time) // 60
         print("Reset in {} min".format(diff))
-        # get remaining rate
-
     elif res.status_code == 404:
         print("Not found")
     elif res.status_code == 200:
         res = res.json()
-        print(res['location'])
+        print(res.get("location"))
