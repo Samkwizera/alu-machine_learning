2a33655bef9e2d0d86833f4d9f333c4b7f136709
Samkwizera <s.ihimbazwe@alustudent.com>
2026-06-22 22:49:14 +0200

    Update API exercise scripts

--- pipeline/apis/1-sentience.py
+++ pipeline/apis/1-sentience.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python3
-"""Find the home planets of all sentient Star Wars species."""
+"""
+    Return the list of names of the home
+    planets of all sentient species.
+"""
 
 import requests
 
 
 def sentientPlanets():
-    """Return home planet names for all sentient species."""
-    url = "https://swapi-api.alx-tools.com/api/species/"
-    planets = []
-
-    while url is not None:
-        response = requests.get(url)
-        data = response.json()
-
-        for species in data.get("results", []):
-            if species.get("designation") == "sentient":
-                homeworld = species.get("homeworld")
-                if homeworld is None:
-                    planets.append("unknown")
-                else:
-                    planet = requests.get(homeworld).json()
-                    planets.append(planet.get("name"))
-
-        url = data.get("next")
-
-    return planets
+    '''
+    Return the list of names of the home
+    planets of all sentient species.
+    '''
+    url = "https://swapi-api.hbtn.io/api/species/?format=json"
+    speciesList = []
+    while url:
+        results = requests.get(url).json()
+        speciesList += results.get('results')
+        url = results.get('next')
+    homePlanets = []
+    for species in speciesList:
+        if species.get('designation') == 'sentient' or \
+           species.get('classification') == 'sentient':
+            url = species.get('homeworld')
+            if url:
+                planet = requests.get(url).json()
+                homePlanets.append(planet.get('name'))
+    return homePlanets
--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
-"""Print the location of a GitHub user."""
 
+
+""" Return list of ships"""
+
+import requests
 import sys
 import time
-import requests
 
 
 if __name__ == "__main__":
-    response = requests.get(sys.argv[1])
+    res = requests.get(sys.argv[1])
 
-    if response.status_code == 404:
+    if res.status_code == 403:
+        rate_limit = int(res.headers.get('X-Ratelimit-Reset'))
+        current_time = int(time.time())
+        diff = (rate_limit - current_time) // 60
+        print("Reset in {} min".format(diff))
+        # get remaining rate
+
+    elif res.status_code == 404:
         print("Not found")
-    elif response.status_code == 403:
-        reset = int(response.headers.get("X-Ratelimit-Reset", 0))
-        minutes = int((reset - time.time()) / 60)
-        if minutes < 0:
-            minutes = 0
-        print("Reset in {} min".format(minutes))
-    else:
-        print(response.json().get("location"))
+    elif res.status_code == 200:
+        res = res.json()
+        print(res['location'])
